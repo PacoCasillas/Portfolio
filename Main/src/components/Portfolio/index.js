fea0ec4e793492677a8dbebed6ce3ececd36b3ed
PacoCasillas <pacocasillasmx@gmail.com>
2023-07-20 12:15:13 -0400

    added own projects

--- Main/src/components/Portfolio/index.js
+++ Main/src/components/Portfolio/index.js
@@ -6,35 +6,48 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'surf-report',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'eventopia',
+      description: 'Full Stack',
+      link: "https://eventopia-ea333846024a.herokuapp.com/",
+      repo: "https://github.com/PacoCasillas/Eventopia"
     },
     {
-      name: 'pastel-puzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'movie-dashboard',
+      description: 'Front End',
+      link: "https://timebytes.github.io/Movie-Dashboard/",
+      repo: "https://github.com/PacoCasillas/Movie-Dashboard"
     },
     {
-      name: 'run-buddy',
-      description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'text-editor',
+      description: 'PWA',
+      link: "https://github.com/PacoCasillas/Text-Editor",
+      repo: "https://github.com/PacoCasillas/Text-Editor"
     },
     {
-      name: 'led-wall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'note-taker',
+      description: 'Express JS',
+      link: "https://francisco-express-note-taker.herokuapp.com/index.html",
+      repo: "https://github.com/PacoCasillas/express-note-taker"
     },
     {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'day-planner',
+      description: 'Third Party APIs',
+      link: "https://pacocasillas.github.io/Daily-Planner/",
+      repo: "https://github.com/PacoCasillas/Daily-Planner"
     },
+    {
+      name: 'password-generator',
+      description: 'JavaScript',
+      link: "https://pacocasillas.github.io/password-generator/",
+      repo: "https://github.com/PacoCasillas/password-generator"
+    },
+    {
+      name: 'gist',
+      description: 'Computer Science',
+      link: "https://gist.github.com/PacoCasillas/856dfd294e6871090b525e3310ec3392",
+    },
+    
+
   ]);
 
   return (
