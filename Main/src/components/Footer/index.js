3328a51d00d1aef77d0dedb2abb14fe48f1d5e7a
PacoCasillas <pacocasillasmx@gmail.com>
2023-07-20 12:15:26 -0400

    changed links in the footer

--- Main/src/components/Footer/index.js
+++ Main/src/components/Footer/index.js
@@ -5,12 +5,16 @@ function Footer() {
   // Replace links with social media profiles
   const icons = [
     {
+      name: "fab fa-linkedin",
+      link: "https://www.linkedin.com/in/francisco-casillas-mx"
+    },
+    {
       name: "fab fa-github",
       link: "https://github.com/PacoCasillas"
     },
     {
-      name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/in/francisco-casillas-mx"
+      name: "fab fa-facebook",
+      link: "https://www.facebook.com/paco.casillas/"
     }
   ]
 
